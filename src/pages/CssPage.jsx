0577ac6a3913908137bab28932dc35d05220e4bc
Jair <jairaceves56@gmail.com>
2023-01-17 19:36:19 -0600

    Css Projects Added

--- src/pages/CssPage.jsx
+++ src/pages/CssPage.jsx
@@ -2,6 +2,107 @@ import { Titulo } from "../components/Titulo"
 
 export const CssPage = () => {
   return (
-    <Titulo titulo='Css'/>
+    <>
+      <Titulo titulo='Css'/>
+
+      <section>
+        <h2 className="text-center mt-36 text-6xl text-white font-mono">Css Proyects</h2>
+        <div className="mt-12">
+          <div className="container-proyects">
+            {/* Nucleus */}
+            <a className="a-tag-container" href="https://jair-vet.github.io/Nucleus_app/" target="_blank">
+              <img
+                className="img-tag-container"
+                src="https://res.cloudinary.com/dytlbyofu/image/upload/v1670014703/Applications%20made/NucleosWallet_zlsb8l.png" 
+                alt="" 
+              />
+            </a>
+            {/* TechPRO */}
+            <a className="a-tag-container" href="https://jair-vet.github.io/TechPRO/" target="_blank">
+              <img
+                className="img-tag-container"
+                src="https://res.cloudinary.com/dytlbyofu/image/upload/v1670014703/Applications%20made/Tech_Pro_ugxttz.png" 
+                alt="" 
+              />
+            </a>
+            {/* Tienda Muebles */}
+            <a className="a-tag-container" href="https://jair-vet.github.io/Tienda-Ecomerce/index.html" target="_blank">
+              <img
+                className="img-tag-container"
+                src="https://res.cloudinary.com/dytlbyofu/image/upload/v1674004820/Applications%20made/Tienda-muebles_zspujs.png" 
+                alt="" 
+              />
+            </a>
+            {/* Arquitectura Bosque */}
+            <a className="a-tag-container" href="https://jair-vet.github.io/ArquitecturaBosque/" target="_blank">
+              <img
+                className="img-tag-container"
+                src="https://res.cloudinary.com/dytlbyofu/image/upload/v1674004822/Applications%20made/Arquitectura_rvzyfj.png" 
+                alt="" 
+              />
+            </a>
+            {/* Login Drop */}
+            <a className="a-tag-container" href="https://jair-vet.github.io/Login-Drop-Effect/" target="_blank">
+              <img
+                className="img-tag-container"
+                src="https://res.cloudinary.com/dytlbyofu/image/upload/v1672159388/Applications%20made/Login-Drop-Effect_wxtd7j.png" 
+                alt="" 
+              />
+            </a>
+            {/* Expanding Card */}
+            <a className="a-tag-container" href="#" target="_blank">
+              <img
+                className="img-tag-container"
+                src="https://res.cloudinary.com/dytlbyofu/image/upload/v1674004824/Applications%20made/Expanding_Card_n8bsj0.png" 
+                alt="" 
+              />
+            </a>
+            {/* Login Card */}
+            <a className="a-tag-container" href="https://jair-vet.github.io/Basic-Login/" target="_blank">
+              <img
+                className="img-tag-container"
+                src="https://res.cloudinary.com/dytlbyofu/image/upload/v1674004824/Applications%20made/Login_Card_bskxgv.png" 
+                alt="" 
+              />
+            </a>
+            {/* Landing Page */}
+            <a className="a-tag-container" href="https://jair-vet.github.io/Landing-Page/" target="_blank">
+              <img
+                className="img-tag-container"
+                src="https://res.cloudinary.com/dytlbyofu/image/upload/v1674004823/Applications%20made/Landing_Page_yhpqxx.png" 
+                alt="" 
+              />
+            </a>
+            {/* 404 astronaut */}
+            <a className="a-tag-container" href="https://jair-vet.github.io/404-astronaut/" target="_blank">
+              <img
+                className="img-tag-container"
+                src="https://res.cloudinary.com/dytlbyofu/image/upload/v1674004818/Applications%20made/404-astronaut_yd8o0z.png" 
+                alt="" 
+              />
+            </a>
+            {/* Login and Signup */}
+            <a className="a-tag-container" href="https://jair-vet.github.io/Login-and-Signup/" target="_blank">
+              <img
+                className="img-tag-container"
+                src="https://res.cloudinary.com/dytlbyofu/image/upload/v1674004816/Applications%20made/Login-and-Signup_xpqwxu.png" 
+                alt="" 
+              />
+            </a>
+            {/* 404 Moon */}
+            <a className="a-tag-container" href="https://jair-vet.github.io/404-moon/" target="_blank">
+              <img
+                className="img-tag-container"
+                src="https://res.cloudinary.com/dytlbyofu/image/upload/v1674004818/Applications%20made/404-mon_smyvek.png" 
+                alt="" 
+              />
+            </a>
+          </div>
+        </div>
+      </section>
+
+
+    </>
+
   )
 }
