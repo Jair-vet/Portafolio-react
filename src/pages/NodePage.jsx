a72b517253adcabd0de61270a1bfe483cbc96af7
Jair <jairaceves56@gmail.com>
2023-01-23 12:00:20 -0600

    Se creo el componente de Proximamente

--- src/pages/NodePage.jsx
+++ src/pages/NodePage.jsx
@@ -1,7 +1,12 @@
+import { Cargando } from "../components/Cargando"
 import { Titulo } from "../components/Titulo"
 
 export const NodePage = () => {
   return (
-    <Titulo titulo='Node Js'/>
+    <>
+      <Titulo titulo='Node Js'/>
+      <Cargando />
+    </>
+  
   )
 }
