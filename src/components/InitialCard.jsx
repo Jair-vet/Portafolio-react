f703e4cb02052975d82f358dc7392769708f1c4a
Jair <jairaceves56@gmail.com>
2023-04-29 13:42:30 -0600

    Agregamos la funcionalidad del Typed

--- src/components/InitialCard.jsx
+++ src/components/InitialCard.jsx
@@ -1,42 +1,49 @@
+import { Typed } from "./Typed"
 
 export const InitialCard = () => {
     return (
         <>
             {/* SECCION INICIO  */}
             <section id="inicio" className="inicio">
-            <div className="max-w-[350px] p-[20px] mt-28 m-auto bg-blackBg text-center rounded-[40px]">
-                <div className="block">
-                    <img 
-                        className="block border-solid border-[8px] border-greenBorder w-[80%] m-auto rounded-[100%]"
-                        src="https://res.cloudinary.com/dytlbyofu/image/upload/v1673887901/Portafolio%20assets/boss_sm6orb.jpg" alt="">
-                        </img>
+                <div className="max-w-[350px] p-[20px] mt-28 m-auto bg-blackBg text-center rounded-[40px]">
+                    <div className="block">
+                        <img 
+                            className="block border-solid border-[8px] border-greenBorder w-[80%] m-auto rounded-[100%]"
+                            src="https://res.cloudinary.com/dytlbyofu/image/upload/v1673887901/Portafolio%20assets/boss_sm6orb.jpg" alt="">
+                            </img>
+                    </div>
+                    <h1 className="font-mono text-[42px] font-bold mt-10">JAIR ACEVES</h1>
+                    <h2 className="text-[15px] font-normal">Informartic enginneer - Fullstack / Web-development Jr</h2>
+                    <div className="inline-block mt-10 mb-16 text-center">
+                        <a 
+                            className="social-icons"
+                            href="https://www.facebook.com/carlos.aseves.9" 
+                            target="_blank"><i className="fa-brands fa-facebook-f"></i>
+                        </a>
+                        <a 
+                            className="social-icons"
+                            href="https://github.com/Jair-vet" 
+                            target="_blank"><i className="fa-brands fa-github"></i>
+                        </a>
+                        <a 
+                            className="social-icons"
+                            href="https://wa.me/+523121555316" 
+                            target="_blank"><i className="fa-brands fa-whatsapp"></i>
+                        </a>
+                        <a 
+                            className="social-icons"
+                            href="https://www.instagram.com/_jair0699/" 
+                            target="_blank"><i className="fa-brands fa-instagram"></i>
+                        </a>
+                    </div>
                 </div>
-                <h1 className="font-mono text-[42px] font-bold mt-10">JAIR ACEVES</h1>
-                <h2 className="text-[15px] font-normal">Informartic enginneer - Fullstack / Web-development Jr</h2>
-                <div className="inline-block mt-10 mb-16 text-center">
-                    <a 
-                        className="social-icons"
-                        href="https://www.facebook.com/carlos.aseves.9" 
-                        target="_blank"><i className="fa-brands fa-facebook-f"></i>
-                    </a>
-                    <a 
-                        className="social-icons"
-                        href="https://github.com/Jair-vet" 
-                        target="_blank"><i className="fa-brands fa-github"></i>
-                    </a>
-                    <a 
-                        className="social-icons"
-                        href="https://wa.me/+523121555316" 
-                        target="_blank"><i className="fa-brands fa-whatsapp"></i>
-                    </a>
-                    <a 
-                        className="social-icons"
-                        href="https://www.instagram.com/_jair0699/" 
-                        target="_blank"><i className="fa-brands fa-instagram"></i>
-                    </a>
+                <div className="mt-4 p-2">
+                    <h1 className="text-4xl font-extrabold">Work Tools </h1>
                 </div>
-            </div>
-        </section>
-    </>
+                <div className="p-2">
+                    <Typed />
+                </div>
+            </section>
+        </>
   )
 }
---
+++ src/components/Typed.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import Typical from 'react-typical'
+ 
+export class Typed extends React.Component {
+  render () {
+    return (
+      <Typical
+        className="text-3xl text-center font-extrabold text-greenBorder"
+        steps={['HTML ', 3000, 'CSS ', 3000, 'Taildwindcss ', 3000, 'JavaScript ', 3000,'ReactJs ', 3000, 
+                'NextJs ', 3000, 'NodeJs ', 3000, 'Postman ', 3000, 'Docker ', 3000, 'GitHub ', 3000, 'SQL ', 3000, 
+                'PWA ', 3000, 'VueJs ', 3000, 'Python ', 3000, 'Django ', 3000,]}
+        loop={Infinity}
+        wrapper="p"
+      />
+    )
+  }
+}
+
