1849f877cf5c91661836ce41f5fecb73a71a45e2
Jair <jairaceves56@gmail.com>
2023-06-15 14:42:58 -0600

    Usando Map agregamos la nueva Data Creada

--- src/pages/VuePage.jsx
+++ src/pages/VuePage.jsx
@@ -1,33 +1,29 @@
 import { Proyect } from "../components/Proyect"
 import { Titulo } from "../components/Titulo"
+import { SeedData } from "../data"
 
 export const VuePage = () => {
+
+  const { VuePage } = SeedData
+
   return (
     <>
       <Titulo titulo='Vue Js'/>
       <section className="mx-auto"> 
         <h2 className="text-center mt-36 text-6xl text-white font-mono">Vue Js Proyects</h2>
         <div className="grid lg:grid-cols-4 md:grid-cols-3 grid-cols-2 gap-8 justify-center mt-12 m-6">
-          {/* DayBook App */}
-          <Proyect 
-            enlace='https://super-dieffenbachia-a85345.netlify.app/#/auth'
-            imagen='https://res.cloudinary.com/dytlbyofu/image/upload/v1670027210/Applications%20made/JournalApp_g39ivy.png'
-          />
-          {/* Map App */}
-          <Proyect 
-            enlace='https://deft-basbousa-5af3b7.netlify.app/#/'
-            imagen='https://res.cloudinary.com/dytlbyofu/image/upload/v1672159389/Applications%20made/MapApp_lbaelz.png'
-          />
-          {/* Credit Card */}
-          <Proyect 
-            enlace='https://jair-vet.github.io/Credit-Card-VUE/'
-            imagen='https://res.cloudinary.com/dytlbyofu/image/upload/v1674004818/Applications%20made/Credit-Card-VUE_vuuraq.png'
-          />
-          {/* Pokemon */}
-          <Proyect 
-            enlace='https://eclectic-starship-d44a61.netlify.app/'
-            imagen='https://res.cloudinary.com/dytlbyofu/image/upload/v1670014137/Applications%20made/PokemonApp_w59fsv.png'
-          />
+          {
+            VuePage.map(proyect => (
+              <a className="a-tag-container" href={ proyect.link } target="_blank" key={ proyect.name }>
+                <img
+                className="img-tag-container"
+                src={ proyect.image } 
+                alt="" 
+                />
+                
+              </a>
+            ))
+          }
           
         </div>
        
